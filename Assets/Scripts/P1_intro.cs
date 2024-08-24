9425192c520466e0f846240c6b70617bade919bc
jorge <a2213332183@alumnos.uat.edu.mx>
2024-08-24 10:12:24 -0600

    Mi primer commit

---
+++ Assets/Scripts/P1_intro.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public class P1_intro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        String a="Bienvenido";
+       Debug.Log(a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
